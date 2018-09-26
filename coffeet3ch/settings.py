1baf519ccd328a7d9efb8f9ac7da5cbd7afa8caf
Matheus Vieira <matheusvieiradepaula@gmail.com>
2018-09-26 18:38:03 -0300

    Update whitenoise config

--- coffeet3ch/settings.py
+++ coffeet3ch/settings.py
@@ -17,8 +17,8 @@ from dj_database_url import parse as dburl
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Quick-start development settings - unsuitable for production
--- coffeet3ch/wsgi.py
+++ coffeet3ch/wsgi.py
@@ -11,9 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffeet3ch.settings')
 
 #application = Cling(get_wsgi_application())
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
+#application.add_files('/path/to/more/static/files', prefix='more-files/')
