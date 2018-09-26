f88caf671f28be9d8348e8940359e76a96781dd2
Matheus Vieira <matheusvieiradepaula@gmail.com>
2018-09-26 18:02:57 -0300

    Update requirements.txt

--- coffeet3ch/settings.py
+++ coffeet3ch/settings.py
@@ -17,7 +17,7 @@ from dj_database_url import parse as dburl
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
